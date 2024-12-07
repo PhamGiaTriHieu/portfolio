8809bc548877f36ce8fbf7ff362956e67495964c
Hieu Pham <phamgiatrihieu@gmail.com>
2024-12-07 21:11:30 +0700

    fix some  bug that can not build

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -49,7 +49,7 @@ const Contact = () => {
           {/* Form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
               <p className="text-white/60">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Aliquid, quisquam.
