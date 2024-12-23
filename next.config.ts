93d402281b82d01bb6cafd12f969b07201e8d425
Hieu Pham <phamgiatrihieu@gmail.com>
2024-12-23 11:58:51 +0700

    Add dockerfile, and auto navigate domain

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,29 @@ const nextConfig: NextConfig = {
     PORT: process.env.NODE_ENV === 'development' ? '6868' : '3000',
     VERSION: 'v1',
   },
+  output: 'standalone',
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          {key: 'Access-Control-Allow-Credentials', value: 'true'},
+          {key: 'Access-Control-Allow-Origin', value: '*'},
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
